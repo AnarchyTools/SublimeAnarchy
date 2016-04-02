c16557a3745d43e153ed5fb157331321ec0aea9c
Drew Crawford <drew@sealedabstract.com>
2016-04-02 00:02:05 -0500

    For some reason, self.sources is now a list of lists
    Not sure why, related to #12 I suppose.  Implement workaround for now.
    
    Ping @dunkelstern

--- package/atpkg/task.py
+++ package/atpkg/task.py
@@ -75,7 +75,7 @@ class LLBuildTask(Task):
 		collectedSources = []
 		import os, fnmatch, glob
 		absRoot = os.path.dirname(self.root_path)
-		for descriptor in self.sources:
+		for descriptor in self.sources[0]:
 			if descriptor.endswith("**.swift"):
 				full_descriptor = os.path.join(absRoot, descriptor)
 				for root, dirs, files in os.walk(os.path.dirname(full_descriptor)):
