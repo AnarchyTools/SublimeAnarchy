4669595d58af85c9a3bb1371f1f23d553c3d8f4b
Drew Crawford <drew@sealedabstract.com>
2016-04-18 00:03:28 -0500

    Add missing stylesheet

---
+++ package/stylesheet.py
@@ -0,0 +1,28 @@
+stylesheet = """<style>
+    html {
+    background-color: #232628;
+    color: #CCCCCC;
+    }
+
+    body {
+    font-size: 14px;
+    font-family: Palatino;
+    }
+    pre {
+    font-size: 13px;
+    font-family: Menlo;
+    }
+
+    a {
+    color: #6699cc;
+    }
+
+    b {
+    color: #cc99cc;
+    }
+
+    h1 {
+    color: #99cc99;
+    font-size: 14px;
+    }
+    </style>"""
